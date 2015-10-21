854b0036a3375193a8348a7ab9d4a34f26314ff2
Stanislav <lers@list.ru>
2015-10-21 21:25:31 +0300

    Chapter 10 'Advanced cart'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020181806) do
+ActiveRecord::Schema.define(version: 20151021160412) do
 
   create_table "cards", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -26,8 +26,9 @@ ActiveRecord::Schema.define(version: 20151020181806) do
   create_table "line_items", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 1
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id"
